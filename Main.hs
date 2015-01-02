695461d86bd87e1dea87b6fb7d11355ffb1e32ce
Stefan Haller <haliner@gmail.com>
2015-01-02 22:05:26 +0100

    Implements first draft of the application.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad.IO.Class
+import System.Process
+import Web.Scotty
+
+htmlFile :: FilePath -> ActionM ()
+htmlFile f = do
+  setHeader "Content-Type" "text/html; charset=utf-8"
+  file f
+
+jsFile :: FilePath -> ActionM ()
+jsFile f = do
+  setHeader "Content-Type" "text/javascript; charset=utf-8"
+  file f
+
+left :: ActionM ()
+left = do
+  liftIO $ callProcess "xdotool" ["key", "--clearmodifier", "Left"]
+  redirect "/"
+
+right :: ActionM ()
+right = do
+  liftIO $ callProcess "xdotool" ["key", "--clearmodifier", "Right"]
+  redirect "/"
+
 main :: IO ()
-main = return ()
+main = scotty 8080 $ do
+  get "/" $ htmlFile "index.html"
+  get "/jquery.js" $ jsFile "jquery.js"
+  get "/jquery.form.js" $ jsFile "jquery.form.js"
+  post "/key/left" left
+  post "/key/right" right
